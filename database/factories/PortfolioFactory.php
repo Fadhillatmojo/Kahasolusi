1cb20d729fd6173241f9ee2777cbe9e142edcb57
Fadhill Atmojo <74691569+Fadhillatmojo@users.noreply.github.com>
2023-11-01 23:39:05 +0700

    Fadhil/fix&feat/Fix bug di portfolio dan menambah fitur see More Portfolio and pagination (#11)
    * fadhil/feat/Penambahan Fitur Login dan Logout
    
    * fadhil/fix/Menambah MiddleWare isGuest dan PreventBackistory
    
    * fadhil/feat/menambah fitur detail portfolio pada dashboard client dan see more page
    
    * fadhil/fix/bug pada dashboard cient
    
    * fadhil/fix/Portfolio Migration, menghapus button visit, portfolio factory
    
    * fadhil/feat/Fitur See More Portfolio dan Pagination

--- database/factories/PortfolioFactory.php
+++ database/factories/PortfolioFactory.php
@@ -20,6 +20,7 @@ class PortfolioFactory extends Factory
         return [
             'portfolio_title'=>$this->faker->sentence(2),
             'portfolio_desc'=>$this->faker->paragraph(),
+            'portfolio_url'=>$this->faker->url(),
             'portfolio_image_url'=>$this->faker->imageUrl(640, 480, 'portfolio', true),
             'admin_id'=>Arr::random([1,2,3])
         ];
