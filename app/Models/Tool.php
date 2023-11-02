f4f8140f3a65fb9e2397296671c4d39695fbf560
User <fadhillatmojo@gmail.com>
2023-11-02 22:41:33 +0700

    fadhil/fix&feat/Tool dan dashboardclientcontroller

--- app/Models/Tool.php
+++ app/Models/Tool.php
@@ -10,6 +10,11 @@ class Tool extends Model
 {
     use HasFactory;
     protected $primaryKey = 'tool_id';
+    protected $fillable = [
+        'tool_image_url',
+        'admin_id'
+    ];
+
     public function Admin(){
         return $this->belongsTo(Admin::class, 'admin_id', 'admin_id');
     }
