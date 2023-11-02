a90aa29e4dfc36aea5d65ca6e6255e3eaef9a848
User <fadhillatmojo@gmail.com>
2023-11-02 22:12:49 +0700

    fadhil/fix&feat/See more porto & fillable di model

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -10,6 +10,13 @@ class Portfolio extends Model
 {
     use HasFactory;
     protected $primaryKey = 'portfolio_id';
+    protected $fillable = [
+        'portfolio_title',
+        'portfolio_desc',
+        'portfolio_url',
+        'portfolio_image_url',
+        'admin_id',
+    ];
     public function Admin(){
         return $this->belongsTo(Admin::class, 'admin_id', 'admin_id');
     }
--- resources/views/component/portfolios.blade.php
+++ resources/views/component/portfolios.blade.php
@@ -4,20 +4,6 @@
         <h2>Our Recent Project</h2>
     </div>
 
-    {{-- <div class="container-portofolios-content">
-        <div class="card" style="width: 32%;flex-shrink: 0;">
-            <div class="card-portfolios">
-            <img src="assets/img/IDMC.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Aplikasi Integrated Development Management Centre (IDMC)</h5>
-                    <div class="portfolios-link">
-                        <a href="#" class="btn btn-primary align-center">Visit Link</a>
-                    </div>
-                </div> 
-            </div>
-        </div>
-    </div> --}}
-
     <div id="project" class="project-area overflow-hidden default-padding">
         <div class="container">
             <div class="project-items-area">
@@ -39,8 +25,10 @@
                                             </div>
                                             
                                             <div class="info">
-                                                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iure repellat iste harum, asperiores excepturi nihil</span>
-                                                <h4><a href="#">Link</a></h4>
+                                                <span>
+                                                    {{ $portfolio->portfolio_desc }}
+                                                </span>
+                                                <h4><a href="{{ $portfolio->portfolio_url }}">Go to link ></a></h4>
                                             </div>
                                         </div>
                                     </div>
