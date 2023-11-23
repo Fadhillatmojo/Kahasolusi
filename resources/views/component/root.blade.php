91df2a8b45d3791b52eb3e7230572b3bedde2652
Fadhill Atmojo <74691569+Fadhillatmojo@users.noreply.github.com>
2023-11-23 14:09:17 +0700

    Fix/styling css (#7)
    * fix:roles
    
    * fix: styling css add style-2.css
    
    * fix: link css

--- resources/views/component/root.blade.php
+++ resources/views/component/root.blade.php
@@ -1,180 +1,186 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <!-- ========== Meta Tags ========== -->
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Kahasolusi - Segala Solusi dalam Genggaman Anda">
 
-        <!-- ========== Page Title ========== -->
-        <title>Kahasolusi | Company Profile</title>
+<head>
+    <!-- ========== Meta Tags ========== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Kahasolusi - Segala Solusi dalam Genggaman Anda">
 
-        <!-- ========== Favicon Icon ========== -->
-        <link rel="shortcut icon" href="assets/img/LogoKahaHead.png" type="image/x-icon">
-        {{-- <link rel="icon" href="data:," type=""> --}}
+    <!-- ========== Page Title ========== -->
+    <title>Kahasolusi | Company Profile</title>
 
-        <!-- ========== Start Stylesheet ========== -->
-        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/themify-icons.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/elegant-icons.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/flaticon-set.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/magnific-popup.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/owl.theme.default.min.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/bootsnav.css') }}" rel="stylesheet" />
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-        <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" />
-        <link href="{{ asset('css/style-custom.css') }}" rel="stylesheet" />
-        <!-- ========== End Stylesheet ========== -->
-        @stack('link')
+    <!-- ========== Favicon Icon ========== -->
+    <link rel="shortcut icon" href="assets/img/LogoKahaHead.png" type="image/x-icon">
+    {{--
+    <link rel="icon" href="data:," type=""> --}}
 
-    </head>
+    <!-- ========== Start Stylesheet ========== -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/themify-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/elegant-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/flaticon-set.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/magnific-popup.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/owl.theme.default.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/bootsnav.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style-custom.css') }}" rel="stylesheet" /> --}}
+    <link href="{{ asset('css/style-2.css') }}" rel="stylesheet" />
+    <!-- ========== End Stylesheet ========== -->
+    @stack('link')
 
-    <body>
-        <!-- Preloader Start -->
-        <div class="se-pre-con"></div>
-        <!-- Preloader Ends -->
+</head>
 
-        <!-- Start Header Top 
+<body>
+    <!-- Preloader Start -->
+    <div class="se-pre-con"></div>
+    <!-- Preloader Ends -->
+
+    <!-- Start Header Top 
         ============================================= -->
-        <div class="top-bar-area multi-content bg-dark text-light">
-            <div class="container">
-                <div class="row align-center">
-                    <div class="col-lg-3 logo">
-                        <a href="/">
-                            <img src="{{ asset('assets/img/KahaLogo.png')}}" class="logo" alt="Logo">
-                        </a>
-                    </div>
-                    <div class="col-lg-9 info item-flex space-between">
+    <div class="top-bar-area multi-content bg-dark text-light">
+        <div class="container">
+            <div class="row align-center">
+                <div class="col-lg-3 logo">
+                    <a href="/">
+                        <img src="{{ asset('assets/img/KahaLogo.png')}}" class="logo" alt="Logo">
+                    </a>
+                </div>
+                <div class="col-lg-9 info item-flex space-between">
+                    <ul>
+                        <li>
+                            <i class="fas fa-clock"></i> Working Hours: 9:00 AM – 5:00 PM
+                        </li>
+                    </ul>
+                    <div class="social">
                         <ul>
                             <li>
-                                <i class="fas fa-clock"></i> Working Hours: 9:00 AM – 5:00 PM
+                                <a href="#">
+                                    <img src="{{ asset('assets/img/icon/ic_baseline-facebook.png') }}">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="{{ asset('assets/img/icon/formkit_instagram.png') }}">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="{{ asset('assets/img/icon/ic_twitter.png') }}">
+                                </a>
                             </li>
                         </ul>
-                        <div class="social">
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        <img src="{{ asset('assets/img/icon/ic_baseline-facebook.png') }}">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <img src="{{ asset('assets/img/icon/formkit_instagram.png') }}">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <img src="{{ asset('assets/img/icon/ic_twitter.png') }}">
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- End Header Top -->
+    </div>
+    <!-- End Header Top -->
 
-        <!-- Header 
+    <!-- Header 
         ============================================= -->
-        <header id="header">
+    <header id="header">
 
-            <div class="container box-nav">
-                <div class="row">
-                    <!-- Start Navigation -->
-                    <nav class="navbar top-less logo-less white navbar-default navbar-fixed dark bootsnav on no-full nav-box no-background d-flex align-items-center">
-                        <div class="container nav-container">
-                            <div class="row">
-                                <!-- Start Header Navigation -->
-                                <div class="navbar-header">
-                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
-                                        <i class="fa fa-bars"></i>
-                                    </button>
-                                    <a class="navbar-brand" href="/">
-                                        <img src="assets/img/KahaLogo.png" class="logo" alt="Logo">
-                                    </a>
-                                </div>
-                                <!-- End Header Navigation -->
+        <div class="container box-nav">
+            <div class="row">
+                <!-- Start Navigation -->
+                <nav
+                    class="navbar top-less logo-less white navbar-default navbar-fixed dark bootsnav on no-full nav-box no-background d-flex align-items-center">
+                    <div class="container nav-container">
+                        <div class="row">
+                            <!-- Start Header Navigation -->
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                    data-target="#navbar-menu">
+                                    <i class="fa fa-bars"></i>
+                                </button>
+                                <a class="navbar-brand" href="/">
+                                    <img src="assets/img/KahaLogo.png" class="logo" alt="Logo">
+                                </a>
+                            </div>
+                            <!-- End Header Navigation -->
 
-                                <!-- Collect the nav links, forms, and other content for toggling -->
-                                
-                                    <div class="collapse navbar-collapse" id="navbar-menu">
-                                        <ul class="nav navbar-nav navbar-center" data-in="fadeInDown" data-out="fadeOutUp">
-                                            <li>
-                                                {{-- nyoba scrool view navbar tapi masih belom --}}
-                                                {{-- <a class="active smooth-menu" href="#header">Home</a> --}}
-                                                <a href="/#header" class="smooth-menu">Home</a>
-                                            </li>
-                                            <li>
-                                                <a href="/#about" class="smooth-menu">About</a>
-                                            </li>
-                                            <li>
-                                                <a href="/#portfolios" class="smooth-menu">Portfolios</a>
-                                            </li>
-                                            <li>
-                                                <a href="/#roles" class="smooth-menu">Roles</a>
-                                            </li>
-                                            <li>
-                                                <a href="/#tools" class="smooth-menu">Tools</a>
-                                            </li>
-                                            <li>
-                                                <a href="/#founders" class="smooth-menu">Founders</a>
-                                            </li>
-                                            <li>
-                                                <a href="/#faqs" class="smooth-menu">FAQs</a>
-                                            </li>
-                                            <li>
-                                                <a href="/#testimonials" class="smooth-menu">Testimonials</a>
-                                            </li>
-                                            <li>
-                                                <a href="/#contact" class="smooth-menu">Contact</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                
-                                <!-- /.navbar-collapse -->
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+
+                            <div class="collapse navbar-collapse" id="navbar-menu">
+                                <ul class="nav navbar-nav navbar-center" data-in="fadeInDown" data-out="fadeOutUp">
+                                    <li>
+                                        {{-- nyoba scrool view navbar tapi masih belom --}}
+                                        {{-- <a class="active smooth-menu" href="#header">Home</a> --}}
+                                        <a href="/#header" class="smooth-menu">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="/#about" class="smooth-menu">About</a>
+                                    </li>
+                                    <li>
+                                        <a href="/#portfolios" class="smooth-menu">Portfolios</a>
+                                    </li>
+                                    <li>
+                                        <a href="/#roles" class="smooth-menu">Roles</a>
+                                    </li>
+                                    <li>
+                                        <a href="/#tools" class="smooth-menu">Tools</a>
+                                    </li>
+                                    <li>
+                                        <a href="/#founders" class="smooth-menu">Founders</a>
+                                    </li>
+                                    <li>
+                                        <a href="/#faqs" class="smooth-menu">FAQs</a>
+                                    </li>
+                                    <li>
+                                        <a href="/#testimonials" class="smooth-menu">Testimonials</a>
+                                    </li>
+                                    <li>
+                                        <a href="/#contact" class="smooth-menu">Contact</a>
+                                    </li>
+                                </ul>
                             </div>
 
+                            <!-- /.navbar-collapse -->
                         </div>
-                    </nav>
-                    <!-- End Navigation -->
-                </div>
+
+                    </div>
+                </nav>
+                <!-- End Navigation -->
             </div>
+        </div>
 
-        </header>
-        <!-- End Header -->
-        
-        @yield('body')
+    </header>
+    <!-- End Header -->
 
-        {{--  start footer  --}}
-        @include('component.footer')
-        {{--  end footer  --}}
-        
-        <!-- jQuery Frameworks and JS
+    @yield('body')
+
+    {{-- start footer --}}
+    @include('component.footer')
+    {{-- end footer --}}
+
+    <!-- jQuery Frameworks and JS
         ============================================= -->
-        <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
-        <script src="{{ asset('assets/js/popper.min.js') }}"></script>
-        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('assets/js/jquery.appear.js') }}"></script>
-        <script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
-        <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
-        <script src="{{ asset('assets/js/modernizr.custom.13711.js') }}"></script>
-        <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
-        <script src="{{ asset('assets/js/wow.min.js') }}"></script>
-        <script src="{{ asset('assets/js/progress-bar.min.js') }}"></script>
-        <script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
-        <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
-        <script src="{{ asset('assets/js/count-to.js') }}"></script>
-        <script src="{{ asset('assets/js/jquery.nice-select.min.js') }}"></script>
-        <script src="{{ asset('assets/js/YTPlayer.min.js') }}"></script>
-        <script src="{{ asset('assets/js/jquery.event.move.js') }}"></script>
-        <script src="{{ asset('assets/js/jquery.twentytwenty.js') }}"></script>
-        <script src="{{ asset('assets/js/bootsnav.js') }}"></script>
-        <script src="{{ asset('assets/js/main.js') }}"></script>
-        @stack('scripts')
-    </body>
+    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.appear.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/modernizr.custom.13711.js') }}"></script>
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/js/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/js/progress-bar.min.js') }}"></script>
+    <script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/count-to.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('assets/js/YTPlayer.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.event.move.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.twentytwenty.js') }}"></script>
+    <script src="{{ asset('assets/js/bootsnav.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    @stack('scripts')
+</body>
+
 </html>
--- resources/views/component/testimonials.blade.php
+++ resources/views/component/testimonials.blade.php
@@ -1,35 +1,37 @@
-    <!-- Start Testimonials Area 
+<!-- Start Testimonials Area 
     ============================================= -->
-    <div id="testimonials" class="testimonials-area overflow-hidden carousel-shadow relative half-bg default-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 offset">
-                    <div class="site-heading text-center">
-                        <h4>Testimonials</h4>
-                        <h2>What’s Client’s Say About Us</h2>
-                        <div class="devider"></div>
-                    </div>
+<div id="testimonials" class="testimonials-area overflow-hidden carousel-shadow relative half-bg default-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 offset">
+                <div class="site-heading text-center">
+                    <h4>Testimonials</h4>
+                    <h2>What’s Client’s Say About Us</h2>
+                    <div class="devider"></div>
                 </div>
             </div>
         </div>
-        <div class="container">
-            <div class="testimonial-items">
-                <div class="row">
-                    <div class="col-lg-10">
-                        <div class="testimonial-carousel owl-carousel owl-theme testi">
-                            <!-- peritem -->
-                            <div class="item">
-                                <div class="card-testi">
-                                    <div class="card-testi-item">
-                                        <img src="assets/img/800x800.png" alt="Thumb">
-                                        <div class="card-testi-item-text">
-                                            <b>
+    </div>
+    <div class="container">
+        <div class="testimonial-items">
+            <div class="row">
+                <div class="col-lg-10">
+                    <div class="testimonial-carousel owl-carousel owl-theme testi">
+                        <!-- peritem -->
+                        <div class="item">
+                            <div class="card-testi">
+                                <div class="card-testi-item">
+                                    <img src="assets/img/800x800.png" alt="Thumb">
+                                    <div class="card-testi-item-text">
+                                        <b>
                                             Solusi Software Berkualitas Tinggi
-                                            </b>
-                                            <p>
-                                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, impedit minus nesciunt est non porro id quidem error corporis modi voluptates repellendus pariatur quibusdam ab nobis facilis quos sequi tenetur?
-                                            </p>
-                                            <div class="provider">
+                                        </b>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, impedit
+                                            minus nesciunt est non porro id quidem error corporis modi voluptates
+                                            repellendus pariatur quibusdam ab nobis facilis quos sequi tenetur?
+                                        </p>
+                                        <div class="provider">
                                             <div class="rating">
                                                 <i class="fas fa-star"></i>
                                                 <i class="fas fa-star"></i>
@@ -38,24 +40,26 @@
                                                 <i class="fas fa-star-half-alt"></i>
                                             </div>
                                             <h5>Mulias Prosva</h5>
-                                        </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <!-- peritem -->
-                            <div class="item">
-                                <div class="card-testi">
-                                    <div class="card-testi-item">
-                                        <img src="assets/img/800x800.png" alt="Thumb">
-                                        <div class="card-testi-item-text">
-                                            <b>
+                        </div>
+                        <!-- peritem -->
+                        <div class="item">
+                            <div class="card-testi">
+                                <div class="card-testi-item">
+                                    <img src="assets/img/800x800.png" alt="Thumb">
+                                    <div class="card-testi-item-text">
+                                        <b>
                                             Solusi Software Berkualitas Tinggi
-                                            </b>
-                                            <p>
-                                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, impedit minus nesciunt est non porro id quidem error corporis modi voluptates repellendus pariatur quibusdam ab nobis facilis quos sequi tenetur?
-                                            </p>
-                                            <div class="provider">
+                                        </b>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, impedit
+                                            minus nesciunt est non porro id quidem error corporis modi voluptates
+                                            repellendus pariatur quibusdam ab nobis facilis quos sequi tenetur?
+                                        </p>
+                                        <div class="provider">
                                             <div class="rating">
                                                 <i class="fas fa-star"></i>
                                                 <i class="fas fa-star"></i>
@@ -64,24 +68,26 @@
                                                 <i class="fas fa-star-half-alt"></i>
                                             </div>
                                             <h5>Mulias Prosva</h5>
-                                        </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <!-- peritem -->
-                            <div class="item">
-                                <div class="card-testi">
-                                    <div class="card-testi-item">
-                                        <img src="assets/img/800x800.png" alt="Thumb">
-                                        <div class="card-testi-item-text">
-                                            <b>
+                        </div>
+                        <!-- peritem -->
+                        <div class="item">
+                            <div class="card-testi">
+                                <div class="card-testi-item">
+                                    <img src="assets/img/800x800.png" alt="Thumb">
+                                    <div class="card-testi-item-text">
+                                        <b>
                                             Solusi Software Berkualitas Tinggi
-                                            </b>
-                                            <p>
-                                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, impedit minus nesciunt est non porro id quidem error corporis modi voluptates repellendus pariatur quibusdam ab nobis facilis quos sequi tenetur?
-                                            </p>
-                                            <div class="provider">
+                                        </b>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, impedit
+                                            minus nesciunt est non porro id quidem error corporis modi voluptates
+                                            repellendus pariatur quibusdam ab nobis facilis quos sequi tenetur?
+                                        </p>
+                                        <div class="provider">
                                             <div class="rating">
                                                 <i class="fas fa-star"></i>
                                                 <i class="fas fa-star"></i>
@@ -90,7 +96,6 @@
                                                 <i class="fas fa-star-half-alt"></i>
                                             </div>
                                             <h5>Mulias Prosva</h5>
-                                        </div>
                                         </div>
                                     </div>
                                 </div>
@@ -98,7 +103,8 @@
                         </div>
                     </div>
                 </div>
-            </div>    
+            </div>
         </div>
     </div>
-    <!-- End Testimonials Area  -->
+</div>
+<!-- End Testimonials Area  -->
