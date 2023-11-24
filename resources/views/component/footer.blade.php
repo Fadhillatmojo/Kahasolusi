c2440f4a6704fe8867dbe1a7f7f19a880da02b00
User <fadhillatmojo@gmail.com>
2023-11-24 17:14:31 +0700

    fix:fonts admin

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -1,88 +1,91 @@
-	<footer class="footer text-light m-0">
-	    <!-- illustration
+<footer class="footer text-light m-0">
+	<!-- illustration
 	    <div class="animate-illustration">
 		   <img src="assets/img/illustration/2.png" alt="illustration">
 	    </div> -->
-	    <!-- End illustration -->
-	    <div class="container">
-		   <div class="f-items default-padding">
-			  <div class="row-footer row-cols-1 row-cols-lg-3 ">
-				 <div class="col-lg-4 col-md-6 item">
+	<!-- End illustration -->
+	<div class="container">
+		<div class="f-items default-padding">
+			<div class="row-footer row-cols-1 row-cols-lg-3 ">
+				<div class="col-lg-4 col-md-6 item">
 					<div class="f-item about">
-					    <img src="assets/img/Logo white.png" class="logo" alt="Logo">
-					    <p>
-					    PT Kaha Solusi Indonesia
-					    </p>
-					    <p>
-						   HEADQUARTER
-						   Pucung 001/001 Tamanmartani Kalasan Sleman Yogyakarta
-						   Telp. 085293037949
-					    </p>
-					    <div class="widget social">
-					    <ul class="link-footer">
-						   <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-						   <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-						   <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-					    </ul>
+						<img src="{{ asset('assets/img/Logo white.png') }}" class="logo" alt="Logo">
+						<p>
+							PT Kaha Solusi Indonesia
+						</p>
+						<p>
+							HEADQUARTER
+							Pucung 001/001 Tamanmartani Kalasan Sleman Yogyakarta
+							Telp. 085293037949
+						</p>
+						<div class="widget social">
+							<ul class="link-footer">
+								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+							</ul>
+						</div>
 					</div>
-					</div>
-				 </div>
-				 <div class="col-lg-2 col-md-6 item">
+				</div>
+				<div class="col-lg-2 col-md-6 item">
 					<div class="f-item link">
-					    <h4 class="widget-title">Quick Link</h4>
-					    <ul style="list-style: none;">
-						   <li>
-							  <a href="/#header"><i class="fas fa-angle-right"></i>Home</a>
-						   </li>
-						   <li>
-							  <a href="/#about"><i class="fas fa-angle-right"></i>About us</a>
-						   </li>
-						   <li>
-							  <a href="/#portfolios"><i class="fas fa-angle-right"></i>Portfolios</a>
-						   </li>
-						   <li>
-							  <a href="/#roles"><i class="fas fa-angle-right"></i>Roles</a>
-						   </li>
-						   <li>
-							  <a href="/#tools"><i class="fas fa-angle-right"></i>Tools</a>
-						   </li>
-						   <li>
-							  <a href="/#founders"><i class="fas fa-angle-right"></i>Founders</a>
-						   </li>
-						   <li>
-							  <a href="/#faqs"><i class="fas fa-angle-right"></i>FAQs</a>
-						   </li>
-						   <li>
-							  <a href="/#contact"><i class="fas fa-angle-right"></i>Contact</a>
-						   </li>
-					    </ul>
+						<h4 class="widget-title">Quick Link</h4>
+						<ul style="list-style: none;">
+							<li>
+								<a href="/#header"><i class="fas fa-angle-right"></i>Home</a>
+							</li>
+							<li>
+								<a href="/#about"><i class="fas fa-angle-right"></i>About us</a>
+							</li>
+							<li>
+								<a href="/#portfolios"><i class="fas fa-angle-right"></i>Portfolios</a>
+							</li>
+							<li>
+								<a href="/#roles"><i class="fas fa-angle-right"></i>Roles</a>
+							</li>
+							<li>
+								<a href="/#tools"><i class="fas fa-angle-right"></i>Tools</a>
+							</li>
+							<li>
+								<a href="/#founders"><i class="fas fa-angle-right"></i>Founders</a>
+							</li>
+							<li>
+								<a href="/#faqs"><i class="fas fa-angle-right"></i>FAQs</a>
+							</li>
+							<li>
+								<a href="/#contact"><i class="fas fa-angle-right"></i>Contact</a>
+							</li>
+						</ul>
 					</div>
-				 </div>
-				 <div class="maps-area">
+				</div>
+				<div class="maps-area">
 					<div class="container">
 						<div class="row">
 							<div class="col-lg-12 col-md-12">
 								<div class="google-maps">
-									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.2180760583174!2d110.4015148!3d-7.766683700000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a59bd9063ef33%3A0x12c3bb6cfe6aced9!2sJl.%20Madukoro%20Raya%20Jl.%20Pringgolayan%20No.4C%2C%20RT.01%2FRW.26%2C%20Dabag%2C%20Condongcatur%2C%20Kec.%20Depok%2C%20Kabupaten%20Sleman%2C%20Daerah%20Istimewa%20Yogyakarta%2055281!5e0!3m2!1sen!2sid!4v1700645480433!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+									<iframe
+										src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.2180760583174!2d110.4015148!3d-7.766683700000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a59bd9063ef33%3A0x12c3bb6cfe6aced9!2sJl.%20Madukoro%20Raya%20Jl.%20Pringgolayan%20No.4C%2C%20RT.01%2FRW.26%2C%20Dabag%2C%20Condongcatur%2C%20Kec.%20Depok%2C%20Kabupaten%20Sleman%2C%20Daerah%20Istimewa%20Yogyakarta%2055281!5e0!3m2!1sen!2sid!4v1700645480433!5m2!1sen!2sid"
+										width="600" height="450" style="border:0;" allowfullscreen=""
+										loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			  </div>
-		   </div>
-	    </div>
-	    <!-- Start Footer Bottom -->
-	    <div class="footer-bottom">
-		   <div class="container">
-			  <div class="footer-bottom-box">
-				 <div class="row">
+			</div>
+		</div>
+	</div>
+	<!-- Start Footer Bottom -->
+	<div class="footer-bottom">
+		<div class="container">
+			<div class="footer-bottom-box">
+				<div class="row">
 					<div class="col-lg-6">
-					    <p>&copy; Copyright 2023. All Rights Reserved by Kahasolusi</p>
+						<p>&copy; Copyright 2023. All Rights Reserved by Kahasolusi</p>
 					</div>
-				 </div>
-			  </div>
-		   </div>
-	    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-	</footer>
+</footer>
