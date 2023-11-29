9adebf323d76a742a5a67a3b35dda19db6581ed1
radityaputriangelita <radityaputriangelita@mail.ugm.ac.id>
2023-11-29 23:58:27 +0700

    fix&feat: layout dan asset gambar hero

--- resources/views/component/hero.blade.php
+++ resources/views/component/hero.blade.php
@@ -1,41 +1,25 @@
-<!-- Start Banner 
+<!-- Start Banner
     ============================================= -->
-    <div class="banner-area top-pad-extra text-regular right-shape content-less">
-        <div id="bootcarousel" class="carousel text-light slide animate_text" data-ride="carousel">
+<div class="banner-area top-pad-extra text-regular right-shape content-less">
+    <div id="bootcarousel" class="carousel text-light slide animate_text" data-ride="carousel">
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="carousel-item active bg-cover" style="background-image: url(assets/img/HeroPict1.png);">
-                    <div class="box-table">
-                        <div class="box-cell shadow gradient">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-lg-8">
-                                        <div class="content">
-                                            <h4 data-animation="animated slideInDown">Connecting the World, One Solution at a Time.</h4>
-                                            <h2 data-animation="animated slideInLeft">Hello! We are Solve Your Technology Business</h2>
-                                            <div class="bottom" data-animation="animated slideInUp">
-                                                <a class="btn btn-theme primary effect btn-md" href="#contact">Contact US</a>
-                                            </div>  
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item bg-cover" style="background-image: url(assets/img/HeroPict1.png);">
-                    <div class="box-table">
-                        <div class="box-cell shadow gradient">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-lg-8">
-                                        <div class="content">
-                                            <h4 data-animation="animated slideInRight">Connecting the World, One Solution at a Time.</h4>
-                                            <h2 data-animation="animated slideInLeft">Hello! We are Solve Your Technology Business</h2>
-                                            <div class="bottom" data-animation="animated slideInUp">
-                                                <a class="btn btn-theme primary effect btn-md" href="#">Contact Us</a>
-                                            </div>  
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="carousel-item active bg-cover" style="background-image: url(assets/img/HeroPict.png);">
+                <div class="box-table">
+                    <div class="box-cell shadow gradient">
+                        <div class="container p-sm-5">
+                            <div class="row">
+                                <div class="col-lg-8">
+                                    <div class="content align-items-center">
+                                        <h4 data-animation="animated slideInDown">Connecting the World, One Solution at
+                                            a Time.</h4>
+                                        <h2 data-animation="animated slideInLeft" class="text-center">Hello! We are
+                                            Solve Your Technology
+                                            Business</h2>
+                                        <div class="bottom" data-animation="animated slideInUp">
+                                            <a class="btn btn-theme primary effect btn-md" href="#contact">Contact
+                                                US</a>
                                         </div>
                                     </div>
                                 </div>
@@ -44,16 +28,61 @@
                     </div>
                 </div>
             </div>
-            <!-- End Wrapper for slides -->
-
-            <!-- Left and right controls -->
-            <a class="left carousel-control light" href="#bootcarousel" data-slide="prev">
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control light" href="#bootcarousel" data-slide="next">
-                <span class="sr-only">Next</span>
-            </a>
-
+            <div class="carousel-item bg-cover" style="background-image: url(assets/img/HeroPict2.png);">
+                <div class="box-table">
+                    <div class="box-cell shadow gradient">
+                        <div class="container p-sm-5">
+                            <div class="row justify-content-start">
+                                <div class="col-lg-9">
+                                    <div class="content">
+                                        <h4 data-animation="animated slideInRight">Connecting the World,
+                                            One Solution at
+                                            a Time.</h4>
+                                        <h2 data-animation="animated slideInLeft">Innovative IT Solutions for Your
+                                            Business Success</h2>
+                                        <div class="bottom" data-animation="animated slideInUp">
+                                            <a class="btn btn-theme primary effect btn-md" href="#">Contact Us</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item bg-cover" style="background-image: url(assets/img/HeroPict3.png);">
+                <div class="box-table">
+                    <div class="box-cell shadow gradient">
+                        <div class="container p-sm-5">
+                            <div class="row justify-content-start">
+                                <div class="col-lg-11">
+                                    <div class="content">
+                                        <h4 data-animation="animated slideInRight">Connecting the World,
+                                            One Solution at
+                                            a Time.</h4>
+                                        <h2 data-animation="animated slideInLeft">Maximizing Your Business
+                                            with Strategic IT Solutions</h2>
+                                        <div class="bottom" data-animation="animated slideInUp">
+                                            <a class="btn btn-theme primary effect btn-md" href="#">Contact Us</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <!-- End Wrapper for slides -->
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control light" href="#bootcarousel" data-slide="prev">
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control light" href="#bootcarousel" data-slide="next">
+            <span class="sr-only">Next</span>
+        </a>
+
     </div>
-    <!-- End Banner -->
+</div>
+<!-- End Banner -->
