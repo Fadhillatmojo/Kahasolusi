12e38018bc59c0701ae004a594a4633c16ff9b40
radityaputriangelita <radityaputriangelita@mail.ugm.ac.id>
2023-11-30 00:05:46 +0700

    feat&fix: version dan css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,20 +4,20 @@
     <div class="container-fluid">
         <div class="container">
             @include('component.about')
-        
+
             @include('component.portfolios')
-            
+
             @include('component.roles')
         </div>
-    
+
         @include('component.tools')
         <div class="container">
             @include('component.founders')
-            
+
             @include('component.faqs')
-            
+
             @include('component.testimonials')
-            
+
             @include('component.contact')
 
         </div>
