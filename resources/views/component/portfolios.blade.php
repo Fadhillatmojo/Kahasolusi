3427eb17c845334d675bf5d798832cdedad94643
radityaputriangelita <radityaputriangelita@mail.ugm.ac.id>
2023-11-30 00:03:42 +0700

    fix:responsif portfolio dan title di see-more

--- resources/views/component/portfolios.blade.php
+++ resources/views/component/portfolios.blade.php
@@ -10,44 +10,42 @@
                 <div class="masonary">
                     <div class="row row-cols-1 row-cols-lg-3">
                         @foreach ($portfolios as $portfolio)
-                        <div class="col d-flex justify-content-center">
-                            <!-- Single Item -->
-                            <div class="pf-item">
-                                <div class="card">
-                                    <div class="project-style-two">
-                                        <div class="cover-info">
-                                            @if (Str::contains($portfolio->portfolio_image_url, ['http://',
-                                            'https://']))
-                                            <img src="{{ $portfolio->portfolio_image_url }}" class="card-img-top"
-                                                alt="portfolio">
-                                            @else
-                                            <img src="{{ asset('storage/portfolios/' . $portfolio->portfolio_image_url) }}"
-                                                class="card-img-top" alt="portfolio">
-                                            @endif
-                                            <div class="card-body">
-                                                <h5 class="card-title" style="word-wrap:break-word">
-                                                    {{ $portfolio->portfolio_title }}
-                                                </h5>
-                                                <p class="text-center mb-0">
-                                                    ({{ $portfolio->portfolio_year }})
-                                                </p>
+                            <div class="col d-flex justify-content-center px-0 py-3 p-sm-3">
+                                <!-- Single Item -->
+                                <div class="pf-item">
+                                    <div class="card m-0 m-sm-2">
+                                        <div class="project-style-two">
+                                            <div class="cover-info">
+                                                @if (Str::contains($portfolio->portfolio_image_url, ['http://', 'https://']))
+                                                    <img src="{{ $portfolio->portfolio_image_url }}"
+                                                        class="card-img-top" alt="portfolio">
+                                                @else
+                                                    <img src="{{ asset('storage/portfolios/' . $portfolio->portfolio_image_url) }}"
+                                                        class="card-img-top" alt="portfolio">
+                                                @endif
+                                                <div class="card-body">
+                                                    <h5 class="card-title" style="word-wrap:break-word">
+                                                        {{ $portfolio->portfolio_title }}
+                                                    </h5>
+                                                    <p class="text-center mb-0">
+                                                        ({{ $portfolio->portfolio_year }})
+                                                    </p>
+                                                </div>
                                             </div>
-                                        </div>
 
-                                        <div class="info">
-                                            <span>
-                                                {{ $portfolio->portfolio_desc }}
-                                            </span>
-                                            @if ($portfolio->portfolio_url != null)
-                                            <h4><a href="{{ $portfolio->portfolio_url }}">Go to link ></a></h4>
-                                            @endif
+                                            <div class="info">
+                                                <span>
+                                                    {{ $portfolio->portfolio_desc }}
+                                                </span>
+                                                @if ($portfolio->portfolio_url != null)
+                                                    <h4><a href="{{ $portfolio->portfolio_url }}">Go to link ></a></h4>
+                                                @endif
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                                <!-- End Single Item -->
                             </div>
-                            <!-- End Single Item -->
-                        </div>
-
                         @endforeach
                     </div>
                     {{-- See more button --}}
