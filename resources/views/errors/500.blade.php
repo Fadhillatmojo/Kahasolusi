724aafe2c3419565f70d409dcea7aaaf6c9ae8e1
User <fadhillatmojo@gmail.com>
2023-12-06 09:27:03 +0700

    fix: responsive and add 500 blade

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,22 @@
+@extends('component.root')
+@section('body')
+<!-- Start 500 -->
+<div class="error-page-area default-padding text-center">
+	<div class="container">
+		<div class="error-box">
+			<div class="row">
+				<div class="col-lg-8">
+					<h1>5<span><img src="{{ asset('assets/img/illustration/10.png') }}" alt="illustration"></span>0
+					</h1>
+					<h2>Bad Request</h2>
+					<p>
+						Household shameless incommode at no objection behaviour. Especially do at he possession
+						insensible sympathize boisterous it. Songs he on an widen me event truth.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- End 500 -->
+@endsection
